58f4b52032f346b21cbeff00f40c5af530d26750
DrakeLu <540659251@qq.com>
2015-10-20 13:54:53 +0800

    add new modules

--- examples/agent.lua
+++ examples/agent.lua
@@ -14,6 +14,58 @@ local client_fd
 
 local player = {}
 
+-- print lua data
+local function dump(obj,oneline)
+    local getIndent, quoteStr, wrapKey, wrapVal, dumpObj
+    getIndent = function(level)
+        return string.rep("\t", level)
+    end
+    quoteStr = function(str)
+        return '"' .. string.gsub(str, '"', '\\"') .. '"'
+    end
+    wrapKey = function(val)
+        if type(val) == "number" then
+            return "[" .. val .. "]"
+        elseif type(val) == "string" then
+            return "[" .. quoteStr(val) .. "]"
+        else
+            return "[" .. tostring(val) .. "]"
+        end
+    end
+    wrapVal = function(val, level)
+        if type(val) == "table" then
+            return dumpObj(val, level)
+        elseif type(val) == "number" then
+            return val
+        elseif type(val) == "string" then
+            return quoteStr(val)
+        else
+            return tostring(val)
+        end
+    end
+    dumpObj = function(obj, level)
+        if type(obj) ~= "table" then
+            return wrapVal(obj)
+        end
+        level = level + 1
+        local tokens = {}
+        tokens[#tokens + 1] = "{"
+        for k, v in pairs(obj) do
+            tokens[#tokens + 1] = getIndent(level) .. wrapKey(k) .. " = " .. wrapVal(v, level) .. ","
+        end
+        tokens[#tokens + 1] = getIndent(level - 1) .. "}"
+
+        local thestr = "\n"
+        if oneline then thestr = " " end
+        return table.concat(tokens, thestr)
+    end
+    return dumpObj(obj, 0)
+end
+
+
+
+
+
 function REQUEST:get()
 	print("get", self.what)
 	local r = skynet.call("SIMPLEDB", "lua", "get", self.what)
@@ -25,58 +77,10 @@ function REQUEST:set()
 	local r = skynet.call("SIMPLEDB", "lua", "set", self.what, self.value)
 end
 
-function REQUEST:handshake()
-	--print "send handshake"
-	return { msg = "Welcome to skynet, I will send heartbeat every 5 sec." }
-end
-
 function REQUEST:getnews()
 	print "require news"
 	local r = skynet.call("SIMPLENEWS", "lua","getnews")
 	return { msg = r }
-end
-
-function REQUEST:login_chatroom()
-	skynet.call("CHATROOM","lua","login",skynet:self())
-end
-
-function REQUEST:logout_chatroom()
-    skynet.call("CHATROOM","lua","logout",skynet:self())
-end
-
-function REQUEST:login()
-	print "request login"
-	player.playerid = self.playerid
-
-	sqlstr = "SELECT * FROM L2.player_basic where playerid = "..player.playerid;
-	local res = skynet.call("MYSQL_SERVICE","lua","query",sqlstr)
-	player.player_basic = res
-
-	sqlstr = "SELECT * FROM L2.equipment where playerid = "..player.playerid;
-	local equip = skynet.call("MYSQL_SERVICE","lua","query",sqlstr)
-	player.equipment = equip
-
-	return { result = 1 }
-end
-
-
-function REQUEST:get_player_data()
-    if self.type == 0 then
-    	if self.playerid == player.playerid then
-    	    return { data = player.player_basic }
-    	else
-    		sqlstr = "SELECT * FROM L2.player_basic where playerid = "..player.playerid;
-			local res = skynet.call("MYSQL_SERVICE","lua","query",sqlstr)
-			player.player_basic = res
-			return { data = res }
-		end
-
-    	-- 0 basic
-    elseif self.type == 1 then
-    	-- 1 fight
-    elseif self.type == 2 then
-    	-- 2 items
-    end
 end
 
 
@@ -86,7 +90,221 @@ function REQUEST:chat()
     return { result = 1 }
 end
 
+function REQUEST:handshake()
+	--print "send handshake"
+	return { msg = "Welcome to skynet, I will send heartbeat every 5 sec." }
+end
+
+function REQUEST:get_player_basic()
+	return { data = player.basic }
+end
+
+function REQUEST:get_player_rank()
+	return { rank = 5 }
+end
+
+
+
+
+function REQUEST:login()
+	player.playerid = self.playerid
+
+	local sqlstr
+
+	  -- playerid 0 : integer
+   --  level 1 : integer
+   --  gold 2 : integer
+   --  diamond 3 : integer
+   --  nickname 4 : string
+   --  last_login_time 5 : string
+   --  create_time 6 : string
+
+	sqlstr = "SELECT playerid,level,gold,diamond,nickname,last_login_time,create_time FROM L2.player_basic where playerid = "..player.playerid;
+	local res = skynet.call("MYSQL_SERVICE","lua","query",sqlstr)
+	player.basic = res[1]
+
+
+	-- sqlstr = "SELECT itemid,itemtype,itemextra,itemcount FROM L2.item where playerid = "..player.playerid;
+	-- local items = skynet.call("MYSQL_SERVICE","lua","query",sqlstr)
+	-- player.items = items
+
+	sqlstr = "SELECT data FROM L2.item_b where playerid = "..player.playerid;
+	local items = skynet.call("MYSQL_SERVICE","lua","query",sqlstr)
+    print ("eeeee"..items[1].data)
+    _,player.items = pcall(load("return "..items[1].data))
+    print ("eeee"..dump(player.items))
+
+	sqlstr = "SELECT * FROM L2.soul where playerid = "..player.playerid;
+	local souls = skynet.call("MYSQL_SERVICE","lua","query",sqlstr);
+    
+    player.souls = {}
+    for i,v in pairs(souls) do
+        local temp = {}
+        temp.soulid = i
+        temp.itemids = {}
+        for k = 1,12 do
+
+        	local str = "pos"..k.."_itemid"
+        	if v[str] ~= nil then
+        		temp.itemids[k] = v[str]
+        	else
+        		temp.itemids[k] = -1
+        	end
+        end
+
+        player.souls[i] = temp  	
+    end
+
+    print (dump(player.souls))
+
+    print ("player "..player.playerid.."is initalized!")
+
+    -- finish task test
+    skynet.fork(function()
+		while true do
+			print "send heartbeat"
+			send_package(send_request("task",{name = themsg.name ,msg = themsg.msg,time = os.date()}))
+			skynet.sleep(1000)
+		end
+	end)
+
+
+
+	return { result = 1 }
+end
+
+
+function REQUEST:get_player_items()
+	return { items = player.items }
+end
+
+function REQUEST:get_rank_data()
+	return { data = { { playerid = 1 , name = "aa" , rank = 1 , score = 182 },{ playerid = 2, name = "bb" , rank = 2 , score = 175} }}
+end
+
+function REQUEST:get_player_soul()
+    return { souls = player.souls }
+end
+
+function REQUEST:set_player_basic()
+	return { result = 1}
+end
+
+
+
+function REQUEST:pass_level()
+	player.basic.gold = player.basic.gold + self.gold
+	player.basic.diamond = player.basic.diamond + self.diamond
+	for _,v in pairs(self.items) do
+		table.insert(player.items,v)
+	end
+	if player.basic.level == self.level then
+        player.basic.level = player.basic.level + 1
+    end
+
+	print(dump(player.items))
+	return { result = 1 }
+end
+
+function REQUEST:set_player_soul()
+	return { result = 1}
+end
+
+function REQUEST:get_tasks()
+	return {
+        tasks = {
+            { taskid = 0,type = 0,description = "first task",percent = 0},
+            { taskid = 1,type = 1,description = "2 task",percent = 50},
+            { taskid = 2,type = 2,description = "3 task",percent = 100},
+            { taskid = 3,type = 3,description = "4 task",percent = 100},
+            { taskid = 4,type = 4,description = "5 task",percent = 100},
+            { taskid = 5,type = 5,description = "6 task",percent = 100},
+    	}
+	}
+end
+
+function REQUEST:create_new_player()
+
+    player = {}
+
+
+    local sqlstr = "SELECT playerid FROM L2.player_basic order by playerid desc limit 1"
+	local newplayerid = skynet.call("MYSQL_SERVICE","lua","query",sqlstr)[1].playerid + 1
+	
+    player.basic = {
+        playerid = newplayerid,
+        nickname = self.nickname..math.random(100000),
+        vip = 0,
+        diamond = 0,
+        gold = 0,
+        create_time = os.date("%Y-%m-%M %X"),
+        level = 1,
+        last_login_time = os.date("%Y-%m-%M %X"),
+        cursoul = 1,
+    }
+
+    print (os.date())
+
+    player.items = {}
+    player.souls = {}
+    player.tasks = {
+            { taskid = 0,type = 0,description = "first task",percent = 0},
+            { taskid = 1,type = 1,description = "2 task",percent = 0},
+            { taskid = 2,type = 2,description = "3 task",percent = 0},
+            { taskid = 3,type = 3,description = "4 task",percent = 0},
+            { taskid = 4,type = 4,description = "5 task",percent = 0},
+            { taskid = 5,type = 5,description = "6 task",percent = 0},
+    	} 
+
+    return { result = 1 , playerid = newplayerid }
+end
+
+--落地数据到数据库
+local function save_to_db()
+    local theres = skynet.call("MYSQL_SERVICE","lua","query","SELECT playerid from L2.player_basic where playerid = "..player.basic.playerid)
+    print("hehehe.."..dump(theres))
+
+    if #theres == 0 then
+    	print ("first save this player !!")
+	    	-- save basic to mysql
+	    local str = "INSERT INTO `L2`.`player_basic` (`playerid`, `level`, `vip`, `gold`, `diamond`, `nickname`, `last_login_time`, `create_time`)" 
+	                .."VALUES ( '$playerid', '$level', '$vip', '$gold', '$diamond', '$nickname', '$last_login_time', '$create_time');"
+	    local sqlstr = string.gsub(str,"%$([%w_]+)",player.basic)
+	  
+	    local res = skynet.call("MYSQL_SERVICE","lua","query",sqlstr)
+
+	    
+	    -- save itemdata to mysql
+	    local itemstr = dump(player.items,true)
+	   -- str = "UPDATE L2.item_b SET data = '"..itemstr.."' where playerid = "..player.basic.playerid;
+	    str = "INSERT INTO L2.item_b (playerid,data) values ('"..player.basic.playerid.."','"..itemstr.."');"
+	   
+	    local res = skynet.call("MYSQL_SERVICE","lua","query",str)
+
+    else
+    	print ("the player is exist,update mysql")
+        local tmp = ""
+        for i,v in pairs(player.basic) do
+        	tmp = tmp..i.."='"..v.."',"
+        end
+        tmp = string.sub(tmp,0,string.len(tmp)-1)
+    	local str = "UPDATE L2.player_basic set "..tmp.."where playerid =".. player.basic.playerid
+	    local res = skynet.call("MYSQL_SERVICE","lua","query",str)
+	
+	    local itemstr = dump(player.items,true)
+	    str = "UPDATE L2.item_b SET data = '"..itemstr.."' where playerid = "..player.basic.playerid;
+	    local res = skynet.call("MYSQL_SERVICE","lua","query",str)
+
+    end
+
+   
+    
+end
+
+
+
 function REQUEST:quit()
+	save_to_db()
 	skynet.call(WATCHDOG, "lua", "close", client_fd)
 end
 
@@ -99,11 +317,9 @@ local function request(name, args, response)
 end
 
 local function send_package(pack)
-    --print ("send pack.."..pack)
+  
 	local package = string.pack(">s2", pack)
-	--socket.write(client_fd, package)
 	socket.write(client_fd, package)
-	--socket.write(client_fd, "12321\n")
 end
 
 
@@ -137,6 +353,7 @@ function CMD.chat(themsg)
     print "CMD chat"
 	send_package(send_request("chatting",{name = themsg.name ,msg = themsg.msg,time = os.date()}))
 end
+
 
 
 function CMD.start(conf)
--- examples/proto.lua
+++ examples/proto.lua
@@ -4,97 +4,230 @@ local proto = {}
 
 proto.c2s = sprotoparser.parse [[
 .package {
-	type 0 : integer
-	session 1 : integer
+    type 0 : integer
+    session 1 : integer
+}
+
+.item {          #物品
+    itemid 0 : integer      #物品id,对应格子
+    itemtype 1 : integer    #物品类型
+    itemextra 2: integer    #物品的额外属性
+    itemcount 3: integer    #物品的数量
+}
+
+.player_basic {    #玩家基础数据
+    playerid 0 : integer    #游戏角色的唯一标识id
+    level 1 : integer       #玩家等级
+    gold 2 : integer        #金钱
+    diamond 3 : integer     #钻石
+    nickname 4 : string     #昵称
+    last_login_time 5 : string    #上次登录时间
+    create_time 6 : string        #创建时间
+}
+
+.rankdata {       #排名信息
+    playerid 0 : integer      #玩家角色id        
+    name 1 : string           #玩家昵称
+    rank 2 : string           #玩家等级
+    score 3 : integer         #分数
+}
+
+.soul {            #魂的信息
+    soulid 0: integer      #魂的id(1~12)
+    itemids 1: *integer    #魂的每个部位对应装备id
 }
 
 handshake 1 {
-	response {
-		msg 0  : string
-	}
+    response {
+        msg 0  : string
+    }
 }
 
 get 2 {
-	request {
-		what 0 : string
-	}
-	response {
-		result 0 : string
-	}
+    request {
+        what 0 : string
+    }
+    response {
+        result 0 : string
+    }
 }
 
 set 3 {
-	request {
-		what 0 : string
-		value 1 : string
-	}
+    request {
+        what 0 : string
+        value 1 : string
+    }
 }
 
 quit 4 {}
 
 getnews 5 {
-	response {
+    response {
         msg 0 : string
     }
 }
 
-chat 6 {
-	request {
-        msg 0 : string 
-        name 1 : string
-	}
-	response {
-        result 1 : integer
-	}
-}
-
-get_player_data 7 {
-	request {
-        playerid 0: integer
-        type 1: string
-	}
-	response {
-        data 0: string
-	}
-}
-
-
-
-get_player_rank 8{
-	request {
-        playerid 0: integer
-    }
-    response {
-        rank 0: integer
-    }
-}
-
-login 9 {
+chat 6 {     #聊天
     request {
-        playerid 0: integer
+        msg 0 : string      #信息
+        name 1 : string     #昵称
     }
     response {
-        result 0: integer
+        result 1 : integer    #结果(1表示成功，0表示失败)
     }
 }
 
+get_player_basic 7 {   #获取玩家基本数据
+    request {
+        playerid 0: integer    #玩家
+    }
+    response {
+        data 0: player_basic
+    }
+}
+
+#获取玩家排名
+get_player_rank 8{     
+    request {
+        playerid 0: integer    #玩家角色id
+    }
+    response {
+        rank 0: integer       #排名
+    }
+}
+
+
+#登录
+login 9 {   
+    request {
+        playerid 0: integer       #角色id
+    }
+    response {
+        result 0: integer        #结果(1:成功 0:失败)
+    }
+}
+
+#获取玩家物品
+get_player_items 10 {  
+    request {
+        start 0 :integer   #起始的index   
+        count 1 :integer   #发多少个
+    }
+    response {
+        items 0 : *item    #物品列表
+    }
+}
+
+#获取排行榜信息
+get_rank_data 11 {
+    request {
+        start 0 : integer    #起始的index(1是第一个)
+        count 1 : integer    #数量
+    }
+
+    response {
+        data 0 : *rankdata  #排名列表
+    }
+}
+
+#获取玩家魂的信息  
+get_player_soul 12 {
+    response {
+        souls 0: *soul    #魂信息的列表
+    }
+}
+
+
+#设置玩家基本信息
+set_player_basic 13 {
+    request {
+        type 0 : string     
+        value 1 : string
+    }
+    response {
+        result 0 : integer 
+    }
+}
+
+#过关
+pass_level 14 {
+    request {
+        level 0 : integer  #第几关
+        items 1 : *item
+        gold 2 : integer
+        diamond 3 :integer
+    }
+    response {
+        result 0 : integer
+    }
+}
+
+#设置灵魂
+set_player_soul 15 {
+    request {
+        souls 0: *soul #设置灵魂
+    }
+    response {  
+        result 0 : integer #1成功 0失败
+    }
+}
+
+.task {
+    taskid 0: integer  #任务id   
+    type 1 :integer    #任务类型
+    description 2: string  #任务描述
+    percent 3: integer    #完成百分比
+}
+
+#任务
+get_tasks 16 {
+    response {
+        tasks 0 : *task
+    }
+}
+
+#创建人物
+create_new_player 17 {
+    request { 
+        nickname 0: string
+    }
+    response {
+        result 0 : integer #1成功0失败
+        playerid 1 : integer
+    }
+}
 
 ]]
 
 proto.s2c = sprotoparser.parse [[
 .package {
-	type 0 : integer
-	session 1 : integer
+    type 0 : integer
+    session 1 : integer
+}
+
+.task {
+    taskid 0: integer  #任务id   
+    type 1 :integer    #任务类型
+    description 2: string  #任务描述
+    percent 3: integer    #完成百分比
 }
 
 heartbeat 1 {}
 
+#聊天返回
 chatting 2 {
-	request {
-	    name 0 : string
+    request {
+        name 0 : string
         msg 1 : string
         time 2 : string 
-	}
+    }
+}
+
+#更新任务
+update_task 3 {
+    request {
+        task 0: task
+    }   
 }
 
 
