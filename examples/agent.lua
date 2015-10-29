df7708a7c4e1485ddbebe014aea2e53e74c859a7
Drake Lu <540659251@qq.com>
2015-10-29 11:41:07 +0800

    fix bug in set_player_soul

--- examples/agent.lua
+++ examples/agent.lua
@@ -301,8 +301,8 @@ function REQUEST:set_fightpower()
 end
 
 function REQUEST:set_player_soul()
-	if not self.souls then return end
-	 
+	if not self.souls then return { result = 0 }end
+
 	for i,v in pairs(self.souls) do
 		player.souls[v.soulid] = v
 	end
