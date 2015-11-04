e22f6a3eea234585acf9e7349ef0f973389de002
Drake Lu <540659251@qq.com>
2015-11-04 23:15:31 +0800

    add ldqueue

---
+++ lualib/skynet/ldqueue.lua
@@ -0,0 +1,39 @@
+local skynet = require "skynet"
+local coroutine = coroutine
+local xpcall = xpcall
+local traceback = debug.traceback
+local table = table
+
+function skynet.ldqueue(callback)
+	local callback = callback
+	local current_thread
+	local ref = 0
+	local thread_queue = {}
+	return function(f, ...)
+		local thread = coroutine.running()
+		if current_thread and current_thread ~= thread then
+			table.insert(thread_queue, thread)
+			skynet.wait()
+			assert(ref == 0)	-- current_thread == thread
+		end
+		current_thread = thread
+
+		ref = ref + 1
+		local ok, err = xpcall(f, traceback, ...)
+		if ok then
+			callback(err)
+		else
+			skynet.error(err)
+		end
+		ref = ref - 1
+		if ref == 0 then
+			current_thread = table.remove(thread_queue,1)
+			if current_thread then
+				skynet.wakeup(current_thread)
+			end
+		end
+		assert(ok,err)	
+	end
+end
+
+return skynet.ldqueue
