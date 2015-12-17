69e82edc1d43a8c76389a8e9bf6875b584d6148a
Drake Lu <540659251@qq.com>
2015-12-17 21:48:20 +0800

    add data

---
+++ gamelogic/lua_for_server/data/image_data.lua
@@ -0,0 +1,14 @@
+---------------------------
+--the data's from an Excel, don't edit it here
+---------------------------
+local value_list = {} 
+value_list[1] = {id = 1, filePath = "CocosRes/role/head_1.png", }
+value_list[2] = {id = 2, filePath = "CocosRes/role/head_2.png", }
+value_list[3] = {id = 3, filePath = "CocosRes/role/head_3.png", }
+value_list[4] = {id = 4, filePath = "CocosRes/role/head_4.png", }
+value_list[5] = {id = 5, filePath = "CocosRes/TaskLayer/shalou.png", }
+value_list[6] = {id = 6, filePath = "CocosRes/TaskLayer/gates.png", }
+value_list[7] = {id = 7, filePath = "CocosRes/TaskLayer/level.png", }
+value_list[8] = {id = 8, filePath = "CocosRes/TaskLayer/part.png", }
+
+return value_list
