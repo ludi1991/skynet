16b84f0e386a9ad1760a40edd5778da52a42fbc0
Drake Lu <540659251@qq.com>
2015-12-18 16:03:23 +0800

    new data

--- gamelogic/lua_for_server/data/image_data.lua
+++ gamelogic/lua_for_server/data/image_data.lua
@@ -10,5 +10,15 @@ value_list[5] = {id = 5, filePath = "CocosRes/TaskLayer/shalou.png", }
 value_list[6] = {id = 6, filePath = "CocosRes/TaskLayer/gates.png", }
 value_list[7] = {id = 7, filePath = "CocosRes/TaskLayer/level.png", }
 value_list[8] = {id = 8, filePath = "CocosRes/TaskLayer/part.png", }
+value_list[11] = {id = 11, filePath = "equipment/1300001.png", }
+value_list[12] = {id = 12, filePath = "equipment/1300002.png", }
+value_list[13] = {id = 13, filePath = "equipment/1300003.png", }
+value_list[14] = {id = 14, filePath = "equipment/1300004.png", }
+value_list[15] = {id = 15, filePath = "equipment/1400001.png", }
+value_list[16] = {id = 16, filePath = "equipment/1400002.png", }
+value_list[17] = {id = 17, filePath = "equipment/1400003.png", }
+value_list[18] = {id = 18, filePath = "equipment/1400004.png", }
+value_list[19] = {id = 19, filePath = "equipment/1400005.png", }
+value_list[20] = {id = 20, filePath = "equipment/1500001.png", }
 
 return value_list
